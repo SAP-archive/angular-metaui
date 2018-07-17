f0e7896a2b6ea711a1a06bd83eaf8d66a6d44054
frantisek Kolar <fkolar@icloud.com>
2018-07-16 21:52:28 -0700

    chore(release): Updated release scripts and added npmPublish script

--- scripts/release.sh
+++ scripts/release.sh
@@ -14,78 +14,45 @@ scriptDir=$(cd $(dirname $0); pwd)
 echo "CDing to $scriptDir"
 cd ${scriptDir}
 
-standardVersionBin=
-
-
-#rm -Rf ${dist}
+rm -Rf ../dist
 
 echo "##### Validating packages"
-#./ci/lint.sh
+./ci/lint.sh
 
 echo "##### Building packages to dist"
-#./ci/build.sh dev
+./ci/build.sh dev
 
 echo "##### Testing packages"
-#./ci/test.sh
+./ci/test.sh
 
-if [ ${args[0]} != "patch" ] &&  [ ${args[0]} != "minor" ] &&  [ ${args[0]} != "major" ]; then
+if [ ${args[0]} != "patch" ] &&  [ ${args[0]} != "minor" ] &&  [ ${args[0]} != "major" ] && [ ${args[0]} != "none" ]; then
 
     echo "Missing mandatory argument: . "
     echo " - Usage: ./release.sh  [type]  "
-    echo "      type: patch | minor | major  "
+    echo "      type: patch | minor | major | none"
     exit 1
 fi
 
 
-echo "Running standard-version to create a release package with ${standardVersionBin} --release-as ${args[0]}"
 
-cd ..
-./node_modules/.bin/standard-version --release-as ${args[0]}
+if [ ${args[0]} != "none" ]; then
+    echo "Running standard-version to create a release package with ${standardVersionBin} --release-as ${args[0]}"
 
-NEW_VERSION=$(node -p "require('./package.json').version")
-echo "Bumping up package(s).json to version ${NEW_VERSION}"
+    cd ..
+    ./node_modules/.bin/standard-version --release-as ${args[0]}
 
-cd ./dist
-perl -p -i -e "s/VERSION_PLACEHOLDER/${NEW_VERSION}/g" $(grep -ril VERSION_PLACEHOLDER .) < /dev/null 2> /dev/null
+    NEW_VERSION=$(node -p "require('./package.json').version")
+    echo "Bumping up package(s).json to version ${NEW_VERSION}"
 
-echo "Tagging Repo with git tag v${NEW_VERSION}"
-git tag v${NEW_VERSION}
+    echo "Tagging Repo with git tag v${NEW_VERSION}"
+    git tag v${NEW_VERSION}
 
-printf "=============== Now go to CHANGELOG.md and check if everything is ok then run following commands:================\n\n"
-printf "  git add . &&  git commit -m \"chore(release): v${NEW_VERSION}\"\n\n"
-printf "  git push --follow-tags origin master\n\n"
-
+    printf "=============== Now go to CHANGELOG.md and check if everything is ok then run following commands:================\n\n"
+    printf "  git add . &&  git commit -m \"chore(release): v${NEW_VERSION}\"\n\n"
+    printf "  git push --follow-tags origin master\n\n"
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+fi
 
 
 
